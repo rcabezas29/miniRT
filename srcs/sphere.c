5d497ecc505fda70053e59f7f6f3922088bfb8ab
Raúl Cabezas González <raul.29cg@gmail.com>
2020-10-28 21:02:37 +0100

    Quitados _s

--- srcs/sphere.c
+++ srcs/sphere.c
@@ -6,7 +6,7 @@
 /*   By: rcabezas <rcabezas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/15 09:55:18 by rcabezas          #+#    #+#             */
-/*   Updated: 2020/10/09 10:42:42 by rcabezas         ###   ########.fr       */
+/*   Updated: 2020/10/28 21:01:40 by rcabezas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,26 +38,4 @@ void    sphere(t_minirt *r, t_object *obj, t_ray cam_ray, t_list *tmp)
 		r->a = s.t2;
         r->obj = tmp->content;
     }
-}
-
-void    sphere_s(t_minirt *r, t_object *obj, t_ray light_ray)
-{
-    t_inter s;
-    
-    s.a = 1;
-    s.b = 2 * (light_ray.origin.x * light_ray.dir.x + light_ray.origin.y *
-        light_ray.dir.y + light_ray.dir.z * light_ray.origin.z);
-    s.c = pow(light_ray.origin.x, 2) + pow(light_ray.origin.y, 2) +
-        pow(light_ray.origin.z, 2) - pow(obj->diameter / 2, 2);
-    s.det = pow(s.b, 2) - (4 * s.a * s.c);
-    if (s.det < 0)
-        return ;
-    s.det = sqrt(s.det);
-    s.a = 2 * s.a;
-    s.t1 = (-s.b - s.det) / s.a;
-    s.t2 = (-s.b + s.det) / s.a;
-    if (s.t1 >= 0 && r->b > s.t1)
-		r->b = s.t1;
-	else if (s.t2 >= 0 && r->b > s.t2)
-		r->b = s.t2;
 }
