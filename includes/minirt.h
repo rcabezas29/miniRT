8630ea89c732af86ea0a852355841a11411d9fb8
Raúl Cabezas González <raul.29cg@gmail.com>
2020-11-04 21:03:26 +0100

    Haciendo bmp 3

--- includes/minirt.h
+++ includes/minirt.h
@@ -6,7 +6,7 @@
 /*   By: rcabezas <rcabezas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/20 11:37:51 by rcabezas          #+#    #+#             */
-/*   Updated: 2020/11/04 20:57:46 by rcabezas         ###   ########.fr       */
+/*   Updated: 2020/11/04 21:03:20 by rcabezas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -159,6 +159,6 @@ void    select_cam(t_minirt *r);
 int     key_press1(int keycode, t_minirt *r);
 
 //bmp.c
-void    create_bmp(t_minirt *r);
+void    create_bmp(t_minirt *r, char *file_name);
 
 #endif
