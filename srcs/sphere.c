73071abab0a69b829b968a96248a7e0f6c431d92
Raul Cabezas gonzalez <rcabezas@c1r11s6.42madrid.com>
2020-11-16 21:13:27 +0100

    Arregladas esferas para que no esten en el centro 2

--- srcs/sphere.c
+++ srcs/sphere.c
@@ -6,7 +6,7 @@
 /*   By: rcabezas <rcabezas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/15 09:55:18 by rcabezas          #+#    #+#             */
-/*   Updated: 2020/11/16 20:38:21 by rcabezas         ###   ########.fr       */
+/*   Updated: 2020/11/16 21:11:50 by rcabezas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@ void    sphere(t_minirt *r, t_object *obj, t_ray cam_ray, t_list *tmp)
     s.d2 = s.a - pow(s.d1, 2);
     if (s.d2 > pow(radio, 2))
         return ;
+    s.d2 = sqrt(s.d2);
     s.x = sqrt(pow(radio, 2) - pow(s.d2, 2));
     s.t1 = s.d1 - s.d2;
     s.t2 = s.d1 - s.d2;
