68fa69f23be7bbb0ea4220a533e9ad6e2e992733
Raúl Cabezas González <raul.29cg@gmail.com>
2020-11-16 20:40:04 +0100

    Arregladas esferas para que no esten en el centro

--- srcs/sphere.c
+++ srcs/sphere.c
@@ -6,7 +6,7 @@
 /*   By: rcabezas <rcabezas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/15 09:55:18 by rcabezas          #+#    #+#             */
-/*   Updated: 2020/10/28 21:01:40 by rcabezas         ###   ########.fr       */
+/*   Updated: 2020/11/16 20:38:21 by rcabezas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,29 @@
 void    sphere(t_minirt *r, t_object *obj, t_ray cam_ray, t_list *tmp)
 {
     t_inter s;
+    float radio;
     
-    s.a = 1;
-    s.b = 2 * (cam_ray.origin.x * cam_ray.dir.x + cam_ray.origin.y *
-        cam_ray.dir.y + cam_ray.dir.z * cam_ray.origin.z);
-    s.c = pow(cam_ray.origin.x, 2) + pow(cam_ray.origin.y, 2) +
-        pow(cam_ray.origin.z, 2) - pow(obj->diameter / 2, 2);
-    s.det = pow(s.b, 2) - (4 * s.a * s.c);
-    if (s.det < 0)
+    radio = obj->diameter / 2;
+    s.sub = resta_vec(obj->position, cam_ray.origin);
+    s.a = vector_length(s.sub);
+    s.d1 = dot_product(s.sub, cam_ray.dir);
+    s.a = pow(s.a, 2);
+    if (s.d1 < 0 && s.a > pow(radio, 2))
         return ;
-    s.det = sqrt(s.det);
-    s.a = 2 * s.a;
-    s.t1 = (-s.b - s.det) / s.a;
-    s.t2 = (-s.b + s.det) / s.a;
-    if (s.t1 >= 0 && r->a > s.t1)
+    s.d2 = s.a - pow(s.d1, 2);
+    if (s.d2 > pow(radio, 2))
+        return ;
+    s.x = sqrt(pow(radio, 2) - pow(s.d2, 2));
+    s.t1 = s.d1 - s.d2;
+    s.t2 = s.d1 - s.d2;
+    if (s.a > pow(radio, 2) && s.t1 < r->a)
     {
-		r->a = s.t1;
+        r->a = s.t1;
         r->obj = tmp->content;
     }
-	else if (s.t2 >= 0 && r->a > s.t2)
+    else if (s.a <= pow(radio, 2) && s.t2 < r->a)
     {
-		r->a = s.t2;
+        r->a = s.t2;
         r->obj = tmp->content;
     }
 }
