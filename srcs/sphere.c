131e7c8c9284a6dfe5291a7b186c488fec72aa01
Raúl Cabezas González <raul.29cg@gmail.com>
2020-11-17 19:04:32 +0100

    Ya queda poco

--- srcs/sphere.c
+++ srcs/sphere.c
@@ -6,7 +6,7 @@
 /*   By: rcabezas <rcabezas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/15 09:55:18 by rcabezas          #+#    #+#             */
-/*   Updated: 2020/11/16 21:11:50 by rcabezas         ###   ########.fr       */
+/*   Updated: 2020/11/17 18:17:43 by rcabezas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,27 @@
 
 void    sphere(t_minirt *r, t_object *obj, t_ray cam_ray, t_list *tmp)
 {
-    t_inter s;
-    float radio;
-    
-    radio = obj->diameter / 2;
-    s.sub = resta_vec(obj->position, cam_ray.origin);
-    s.a = vector_length(s.sub);
-    s.d1 = dot_product(s.sub, cam_ray.dir);
-    s.a = pow(s.a, 2);
-    if (s.d1 < 0 && s.a > pow(radio, 2))
-        return ;
-    s.d2 = s.a - pow(s.d1, 2);
-    if (s.d2 > pow(radio, 2))
-        return ;
-    s.d2 = sqrt(s.d2);
-    s.x = sqrt(pow(radio, 2) - pow(s.d2, 2));
-    s.t1 = s.d1 - s.d2;
-    s.t2 = s.d1 - s.d2;
-    if (s.a > pow(radio, 2) && s.t1 < r->a)
+    t_inter	s;
+
+	s.sub = resta_vec(cam_ray.origin, obj->position);
+	s.a = dot_product(cam_ray.dir, cam_ray.dir);
+	s.b = 2 * dot_product(cam_ray.dir, s.sub);
+	s.c = dot_product(s.sub, s.sub) - pow(obj->diameter / 2, 2);
+	s.det = pow(s.b, 2) - (4 * s.a * s.c);
+	if (s.det < 0)
+		return ;
+	s.det = sqrt(s.det);
+	s.a = 2 * s.a;
+	s.t1 = (-s.b - s.det) / s.a;
+	s.t2 = (-s.b + s.det) / s.a;
+	if (s.t1 >= 0 && r->a > s.t1)
     {
-        r->a = s.t1;
+		r->a = s.t1;
         r->obj = tmp->content;
     }
-    else if (s.a <= pow(radio, 2) && s.t2 < r->a)
-    {
-        r->a = s.t2;
+    else if (s.t2 >= 0 && r->a > s.t2)
+	{
+		r->a = s.t2;
         r->obj = tmp->content;
     }
 }
